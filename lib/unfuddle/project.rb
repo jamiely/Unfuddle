157e7b404d9f81df2a3468e9911dce3f43e6900b
Jamie Ly <jamie.ly@gmail.com>
2012-07-06 16:09:22 -0400

    Added time_invested method to return time_invested report based on selected project.

--- lib/unfuddle/project.rb
+++ lib/unfuddle/project.rb
@@ -1,3 +1,5 @@
+require 'chronic'
+
 module Unfuddle
   class Project < ActiveResource::Base    
     def tickets(params = {})
@@ -13,6 +15,14 @@ module Unfuddle
     # cannot be requested together. This will obviously take awhile.
     def time_entries
       tickets.collect(&:time_entries).reduce(&:concat)
+    end
+
+    def time_invested(params = {})
+      params = {
+        :start_date => Chronic.parse('1 week ago').strftime("%Y/%m/%d"),
+        :end_date => Time.now.strftime("%Y/%m/%d")
+        }.merge(params)
+      TimeInvested.find :one, :from => "/projects/#{id}/time_invested", :params => params
     end
   end
 end
